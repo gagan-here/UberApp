c11df08b390a33025cdf9b8033bae536165629a5
Gagan Basnet <chatwidmeah@gmail.com>
2024-12-15 00:14:22 +0545

    added query to find ten nearest drivers

--- src/main/java/com/uber/uberapp/repositories/DriverRepository.java
+++ src/main/java/com/uber/uberapp/repositories/DriverRepository.java
@@ -1,8 +1,27 @@
 package com.uber.uberapp.repositories;
 
 import com.uber.uberapp.entities.Driver;
+import java.util.List;
+import org.locationtech.jts.geom.Point;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Below is spatial functions used in geospatial databases like PostgreSQL with the PostGIS
+ * extension <br>
+ * <b> ST_Distance(point1, point2) </b> - This function calculates the distance between two
+ * geographic points, point1 and point2 <br>
+ * <b> ST_DWithin(point1, point2, 10000) </b> - This is a boolean function that checks whether
+ * point1 and point2 are within a specified distance
+ */
 @Repository
-public interface DriverRepository extends JpaRepository<Driver, Long> {}
+public interface DriverRepository extends JpaRepository<Driver, Long> {
+  @Query(
+      "SELECT d.*, ST_Distance(d.current_location, :pickupLocation) AS distance "
+          + "FROM drivers as d "
+          + "where available = true AND ST_DWithin(d.curent_location, :pickupLocation, 10000) "
+          + "ORDER BY distance "
+          + "LIMIT 10")
+  List<Driver> findTenNearestDrivers(Point pickupLocation);
+}
--- src/main/java/com/uber/uberapp/strategies/impl/DriverMatchingNearestDriverStrategy.java
+++ src/main/java/com/uber/uberapp/strategies/impl/DriverMatchingNearestDriverStrategy.java
@@ -2,14 +2,20 @@ package com.uber.uberapp.strategies.impl;
 
 import com.uber.uberapp.entities.Driver;
 import com.uber.uberapp.entities.RideRequest;
+import com.uber.uberapp.repositories.DriverRepository;
 import com.uber.uberapp.strategies.DriverMatchingStrategy;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class DriverMatchingNearestDriverStrategy implements DriverMatchingStrategy {
+
+  private final DriverRepository driverRepository;
+
   @Override
   public List<Driver> findMatchingDriver(RideRequest rideRequest) {
-    return List.of();
+    return driverRepository.findTenNearestDrivers(rideRequest.getPickupLocation());
   }
 }
