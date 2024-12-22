c0d04e4d34e7229bff5587f711f038f8c8a5c41d
Gagan Basnet <chatwidmeah@gmail.com>
2024-12-23 00:21:14 +0545

    completed implementation of calculating distance and added code to calucalte fare in RideFareSurgePricingFareCalculationStrategy

--- src/main/java/com/uber/uberapp/services/impl/DistanceServiceOSRMImpl.java
+++ src/main/java/com/uber/uberapp/services/impl/DistanceServiceOSRMImpl.java
@@ -1,6 +1,8 @@
 package com.uber.uberapp.services.impl;
 
 import com.uber.uberapp.services.DistanceService;
+import java.util.List;
+import lombok.Data;
 import org.locationtech.jts.geom.Point;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
@@ -13,14 +15,31 @@ public class DistanceServiceOSRMImpl implements DistanceService {
   @Override
   public double calculateDistance(Point src, Point dest) {
 
-    RestClient.builder()
-        .baseUrl(OSRM_API_BASE_URL)
-        .build()
-        .get()
-        .uri("{}, {};{},{}", src.getX(), src.getY(), dest.getY(), dest.getY())
-        .retrieve()
-        .body();
+    try {
+      String uri = src.getX() + "," + src.getY() + ";" + dest.getX() + "," + dest.getY();
 
-    return 0;
+      OSRMResponseDto responseDto =
+          RestClient.builder()
+              .baseUrl(OSRM_API_BASE_URL)
+              .build()
+              .get()
+              .uri(uri)
+              .retrieve()
+              .body(OSRMResponseDto.class);
+
+      return responseDto.getRoutes().get(0).getDistance() / 1000.0;
+    } catch (Exception e) {
+      throw new RuntimeException("Error getting data from OSRM" + e.getMessage());
+    }
   }
+}
+
+@Data
+class OSRMResponseDto {
+  private List<OSRMRoute> routes;
+}
+
+@Data
+class OSRMRoute {
+  private Double distance;
 }
--- src/main/java/com/uber/uberapp/strategies/impl/RideFareSurgePricingFareCalculationStrategy.java
+++ src/main/java/com/uber/uberapp/strategies/impl/RideFareSurgePricingFareCalculationStrategy.java
@@ -1,11 +1,23 @@
 package com.uber.uberapp.strategies.impl;
 
 import com.uber.uberapp.entities.RideRequest;
+import com.uber.uberapp.services.DistanceService;
 import com.uber.uberapp.strategies.RideFareCalculationStrategy;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class RideFareSurgePricingFareCalculationStrategy implements RideFareCalculationStrategy {
+  private final DistanceService distanceService;
+  private static final double SURGE_FACTOR = 2;
+
   @Override
   public double calculateFare(RideRequest rideRequest) {
-    return 0;
+    double distance =
+        distanceService.calculateDistance(
+            rideRequest.getPickupLocation(), rideRequest.getDropOffLocation());
+
+    return distance * RIDE_FARE_MULTIPLIER * SURGE_FACTOR;
   }
 }
