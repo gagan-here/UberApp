5db7c4f67140cdeeb4ef2ea68ffef6e2e6feb9e6
Gagan Basnet <chatwidmeah@gmail.com>
2024-12-22 23:59:27 +0545

    added partial implementation to calculate distance using OSRM API

--- src/main/java/com/uber/uberapp/services/impl/DistanceServiceOSRMImpl.java
+++ src/main/java/com/uber/uberapp/services/impl/DistanceServiceOSRMImpl.java
@@ -3,12 +3,24 @@ package com.uber.uberapp.services.impl;
 import com.uber.uberapp.services.DistanceService;
 import org.locationtech.jts.geom.Point;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
 
 @Service
 public class DistanceServiceOSRMImpl implements DistanceService {
+  private static final String OSRM_API_BASE_URL =
+      "https://router.project-osrm.org/route/v1/driving/";
+
   @Override
   public double calculateDistance(Point src, Point dest) {
-    // TODO Call the third pary api called OSRM to fetch the distance
+
+    RestClient.builder()
+        .baseUrl(OSRM_API_BASE_URL)
+        .build()
+        .get()
+        .uri("{}, {};{},{}", src.getX(), src.getY(), dest.getY(), dest.getY())
+        .retrieve()
+        .body();
+
     return 0;
   }
 }
