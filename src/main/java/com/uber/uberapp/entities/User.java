cf256e5e1eeded59c9db0751beefd3cf544344eb
Gagan Basnet <chatwidmeah@gmail.com>
2025-01-17 02:02:37 +0545

    added implementation for jwtfilter, jwtservice and userservice

--- src/main/java/com/uber/uberapp/entities/User.java
+++ src/main/java/com/uber/uberapp/entities/User.java
@@ -12,9 +12,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Index;
 import jakarta.persistence.Table;
+import java.util.Collection;
 import java.util.Set;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Table(
@@ -22,7 +26,7 @@ import lombok.Setter;
     indexes = {@Index(name = "idx_user_email", columnList = "email")})
 @Getter
 @Setter
-public class User {
+public class User implements UserDetails {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -38,4 +42,14 @@ public class User {
   @ElementCollection(fetch = FetchType.LAZY)
   @Enumerated(EnumType.STRING)
   private Set<Role> roles;
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return roles.stream().map(role -> new SimpleGrantedAuthority("ROLE_" + role.name())).toList();
+  }
+
+  @Override
+  public String getUsername() {
+    return "";
+  }
 }
---
+++ src/main/java/com/uber/uberapp/services/UserService.java
@@ -0,0 +1,28 @@
+package com.uber.uberapp.services;
+
+import com.uber.uberapp.entities.User;
+import com.uber.uberapp.exceptions.ResourceNotFoundException;
+import com.uber.uberapp.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+
+  UserRepository userRepository;
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    return null;
+  }
+
+  public User getUserById(Long id) {
+    return userRepository
+        .findById(id)
+        .orElseThrow(() -> new ResourceNotFoundException("User not found with id: " + id));
+  }
+}
