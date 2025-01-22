99c2b14fb019722d9e8d44a4739a6afb1ae3d9c4
Gagan Basnet <chatwidmeah@gmail.com>
2025-01-23 01:15:42 +0545

    added exception handler fo authentication

--- src/main/java/com/uber/uberapp/advices/GlobalExceptionHandler.java
+++ src/main/java/com/uber/uberapp/advices/GlobalExceptionHandler.java
@@ -2,10 +2,13 @@ package com.uber.uberapp.advices;
 
 import com.uber.uberapp.exceptions.ResourceNotFoundException;
 import com.uber.uberapp.exceptions.RunTimeConflictException;
+import io.jsonwebtoken.JwtException;
 import java.util.List;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -26,6 +29,27 @@ public class GlobalExceptionHandler {
       RunTimeConflictException exception) {
     ApiError apiError =
         ApiError.builder().status(HttpStatus.CONFLICT).message(exception.getMessage()).build();
+    return buildErrorResponseEntity(apiError);
+  }
+
+  @ExceptionHandler(AuthenticationException.class)
+  public ResponseEntity<ApiResponse<?>> handleAuthenticationException(AuthenticationException ex) {
+    ApiError apiError =
+        ApiError.builder().status(HttpStatus.UNAUTHORIZED).message(ex.getMessage()).build();
+    return buildErrorResponseEntity(apiError);
+  }
+
+  @ExceptionHandler(JwtException.class)
+  public ResponseEntity<ApiResponse<?>> handleJwtException(JwtException ex) {
+    ApiError apiError =
+        ApiError.builder().status(HttpStatus.UNAUTHORIZED).message(ex.getMessage()).build();
+    return buildErrorResponseEntity(apiError);
+  }
+
+  @ExceptionHandler(AccessDeniedException.class)
+  public ResponseEntity<ApiResponse<?>> handleAccessDeniedException(AccessDeniedException ex) {
+    ApiError apiError =
+        ApiError.builder().status(HttpStatus.FORBIDDEN).message(ex.getMessage()).build();
     return buildErrorResponseEntity(apiError);
   }
 
